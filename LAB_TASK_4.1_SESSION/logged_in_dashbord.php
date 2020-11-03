b507a80a10800c0c2755b060d07706bf0fa1d653
Tanjil Alam Sohan <tanjilsohan67@gmail.com>
2020-11-04 01:39:38 +0600

    Logged In Dashbord

---
+++ LAB_TASK_4.1_SESSION/logged_in_dashbord.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Dashbord</title>
+	<style>
+		li.header{
+            display: inline;
+           }
+
+      table{
+      	border: 1px solid black;
+      	border-collapse: collapse;
+      }
+
+      td{
+        	border-bottom: 1px solid black;
+        }
+        .middle{
+
+        	border-right: 1px solid black;
+        }
+	</style>
+</head>
+<body>
+   <table width="100%">
+   	<tr style="height: 30px">
+		<td width="300px"><img align="left" src="logo.PNG" height="60px" width="100px"></td>
+		<td width="500px"></td>
+		<td >
+			<ul type="none" >
+				<li class="header"><a href="Public_home.php">Home</a></li>
+				<li class="header"><a href="login.php">Login</a></li>
+				<li class="header"><a href="registration.php">Registration</a></li>
+			</ul>
+		</td>
+	</tr>
+	<tr>
+		<td class="middle"  height="360px">
+			<ul type="none" >
+				 
+				<li><h3>Accounts</h3></li>
+			</ul>
+			<ul>
+				<li><a href="logged_in_dashbord.php">Dashbord</a></li>
+				<li><a href="view_profile.php">View Profile</a></li>
+				<li><a href="edit_profile.php">Edit Profile</a></li>
+				<li><a href="change_profile_picture.php">Change profile Picture</a></li>
+				<li><a href="change_password.php">Change Password</a></li>
+				<li><a href="logout.php"></a>Logout</li>
+			</ul>
+		</td>
+		<td colspan="2">
+			<h2>Welcome</h2>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3" align="center" colspan="3"><h3>Copy Right</h3></td>
+	</tr>
+   </table>
+</body>
+</html>
