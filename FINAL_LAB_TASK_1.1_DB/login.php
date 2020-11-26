7178630e4f357a88aa8bb459656f8fda3d5a5c31
Tanjil Alam Sohan <tanjilsohan67@gmail.com>
2020-11-26 21:35:43 +0600

    login Page

---
+++ FINAL_LAB_TASK_1.1_DB/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+ <div>
+ 	<table>
+ 		<fieldset style="width: 200px">
+ 			<legend>LOG IN</legend>
+ 			user name :<input type="text" name="uname" value=""><br><br>
+ 			password:<input type="password" name="password" value="">
+ 		</fieldset>
+ 	</table>
+ </div>
+</body>
+</html>
