2ad4e95ada20ae0ccd412c536a786395cbb51e9e
Tanjil Alam Sohan <tanjilsohan67@gmail.com>
2020-11-27 03:28:54 +0600

    Create Operation completed

---
+++ FINAL_LAB_TASK_1.1_DB/interface/profile_page_home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>profile home</title>
+</head>
+<body>
+   <h3>Welcome to home page</h3>
+   
+    <a href="create.php">Create New User</a> |
+	<a href="profile.php">Profile</a> |
+	<a href="userlist.php">User List</a> |
+	<a href="../php/logout.php">logout</a>
+
+   
+</body>
+</html>
---
+++ FINAL_LAB_TASK_1.1_DB/interface/user_list.php
@@ -0,0 +1,32 @@
+<?php
+
+
+if(isset($_GET['msg'])){
+		
+		if($_GET['msg'] == "user_inserted"){
+
+			echo "username inserted successfully";
+		}
+
+		/*if($_GET['msg'] == "null_username"){
+			echo "Username field is empty...";
+		}
+
+		if($_GET['msg'] == "null_password"){
+			echo "Password field is empty...";
+		}*/
+	}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>user list</title>
+</head>
+<body>
+   <h3>wann see user list??</h3>
+</body>
+</html>
