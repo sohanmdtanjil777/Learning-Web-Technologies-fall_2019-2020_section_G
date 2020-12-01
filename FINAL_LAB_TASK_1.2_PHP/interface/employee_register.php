9a37621a8db2c64b5ad78d89ecbeac1a651a040f
Tanjil Alam Sohan <tanjilsohan67@gmail.com>
2020-12-01 09:00:06 +0600

    commit at 8:38 AM

---
+++ FINAL_LAB_TASK_1.2_PHP/interface/employee_register.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+<div>
+	<form>
+		<fieldset>
+			<legend>New Employee</legend>
+			<table>
+				<tr>
+					<td>Employee Name</td>
+					<td><input type="text" name="ename"></td>
+				</tr>
+
+				<tr>
+					<td>Company Name</td>
+					<td><input type="text" name="coname"></td>
+				</tr>
+
+				<tr>
+					<td>Contact No</td>
+					<td><input type="text" name="contact"></td>
+				</tr>
+
+				<tr>
+					<td>User name</td>
+					<td><input type="text" name="uname"></td>
+				</tr>
+
+				<tr>
+					<td>Password</td>
+					<td><input type="Password" name="password"></td>
+				</tr>
+
+				<tr>
+					<td>User Type</td>
+					<td><input type="text" name="utype"></td>
+				</tr>
+
+			</table>
+		</fieldset>
+	</form>
+</div>
+</body>
+</html>
